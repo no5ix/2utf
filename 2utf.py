5ba488820991e2677afaec29ed2ab0447e8bb383
hulinhong <1145263208@qq.com>
2019-01-28 02:06:39 +0800

    add new readme

--- 2utf.py
+++ 2utf.py
@@ -4,10 +4,11 @@
 import os, argparse, textwrap, time
 import codecs
 import chardet
+import shutil
 
 
 DEFAULT_CONF = {
-    'check_ext_tuple' : ['cpp', 'h', 'uproject', 'cs', 'txt'],
+    'check_ext' : ['cpp', 'h', 'uproject', 'cs'],
     # 'size_limit': 100 * 1024 ** 2,  # if the file is larger than this size limit, we could skip it. default 100MB
     'codec_chain': ['ascii', 'utf_8_sig', 'chardet'],
     # We will try elements in this list sequentially.
@@ -19,13 +20,13 @@ DEFAULT_CONF = {
     # We have to set a minimum threshold. Only those target_encoding results returned by chartdet that are above
     # that threshold level would be accepted.
     # See https://github.com/x1angli/cvt2utf/issues/4 for further details
-    'base_dir': './',
-    'bak_dir_name_prefix': './2utf_bak_' + str(time.strftime("%Y_%m_%d__%H_%M_%S")) + '/',
+    'check_dir': './',
+    'bak_dir_name_prefix': '2utf_bak_',
 }
 
 
 def cli():
-    parser = argparse.ArgumentParser(
+    cvt_parser = argparse.ArgumentParser(
         description="A tool that converts non-UTF-encoded text files UTF-8 encoded files.",
         epilog=textwrap.dedent('''
             You can use this tool to remove BOM all code files from your source code repo, 
@@ -33,9 +34,39 @@ def cli():
             '''),
     )
 
-    parser.add_argument(
+    # subparsers = parser.add_subparsers(dest='cmd')
+    #
+    # cvt_parser = subparsers.add_parser('convert', help=textwrap.dedent('''
+    #         The main command that converts {DEFAULT_CONF['check_ext']} files into UTF8-encoded ones.
+    #         '''))
+
+    cvt_parser.set_defaults(func=cvt_codec_main)
+
+    cvt_parser.add_argument(
+        '-d',
+        '--dir',
+        dest='check_dir',
+        # action='store_true',
+        default=DEFAULT_CONF['check_dir'],
+        help=textwrap.dedent('''
+            The path pointing to the file or directory.
+        '''),
+    )
+
+    cvt_parser.add_argument(
+        '-c',
+        '--cleanbak',
+        dest='clean_bak',
+        action='store_true',
+        default=False,
+        help=textwrap.dedent('''
+            Clean all bak files.
+        '''),
+    )
+
+    cvt_parser.add_argument(
         '-n',
-        'nobak',
+        '--nobak',
         dest='no_bak',
         action='store_true',
         default=False,
@@ -44,7 +75,7 @@ def cli():
         '''),
     )
 
-    tgt_group = parser.add_mutually_exclusive_group()  # This group will be used to specify the target encoding
+    tgt_group = cvt_parser.add_mutually_exclusive_group()  # This group will be used to specify the target encoding
 
     tgt_group.add_argument(
         '-t',
@@ -68,20 +99,53 @@ def cli():
             '''),
     )
 
-    args = parser.parse_args()
+    args = cvt_parser.parse_args()
 
-    base = DEFAULT_CONF['base_dir']
-    if os.path.isdir(base):
-        walk_dir(base, args)
+    # if args.cmd is None:
+    #     import sys
+    #     args = cvt_parser.parse_args(['convert'] + sys.argv[1:])
+
+    args.func(args)
+
+def clean_bak_main(cur_check_dir):
+    # # base = args.base
+    # base = args.check_dir
+    #
+    # if not os.path.exists(base):
+    #     return
+
+    if not os.path.isdir(cur_check_dir):
+        print "The " + cur_check_dir + " is not a directory"
+        return
+
+    dirs = os.listdir(cur_check_dir)
+    for sub_dir_name in dirs:
+        if sub_dir_name.startswith(DEFAULT_CONF['bak_dir_name_prefix']):
+            shutil.rmtree(sub_dir_name, ignore_errors=True)
+
+def cvt_codec_main(args):
+    cur_check_path = args.check_dir
+    if not os.path.exists(cur_check_path):
+    # if not os.path.exists(cur_check_path.replace('\\', '/')):
+        print "the path '" + cur_check_path + "' that you entered is not exists."
+        print "tip 1 : use slash / but don't use backslash \\"
+        print "tip 2 : As much as possible, use English path name"
+        return
+
+    if args.clean_bak:
+        clean_bak_main(cur_check_path)
     else:
-        convert_file(base, args)
+        if os.path.isdir(cur_check_path):
+            walk_dir(cur_check_path, args)
+        else:
+            convert_file(cur_check_path, args)
 
 def walk_dir(base, args):
     for root, dirs, files in os.walk(base):
         for name in files:
 
             extension = os.path.splitext(name)[1][1:].strip().lower()
-            if extension in DEFAULT_CONF['check_ext_tuple']:
+            if extension in DEFAULT_CONF['check_ext']:
                 fullname = os.path.join(root, name)
                 try:
                     convert_file(fullname, args)
@@ -102,6 +166,7 @@ def convert_file(filename, args):
 
     src_codec = detect_codec(file_bytes, filename)
     if src_codec is None:
+        print "Unable to recognize the codec for " + filename + "."
         return
 
     if src_codec == 'ascii':
@@ -113,11 +178,14 @@ def convert_file(filename, args):
     try:
         file_str = file_bytes.decode(src_codec)
     except UnicodeDecodeError as e:
+        print "Unable to open " + filename + " with codec " + src_codec + "."
         return
 
     if not args.no_bak:
         file_name_tuple = os.path.split(filename)
-        new_dir_name = DEFAULT_CONF['bak_dir_name_prefix'] + file_name_tuple[0].replace('./', '')
+        new_dir_name = './' + DEFAULT_CONF['bak_dir_name_prefix'] +\
+                       str(time.strftime("%Y_%m_%d__%H_%M_%S")) +\
+                       '/' + file_name_tuple[0].replace('./', '')
         new_dir_file_name = new_dir_name + '/' + file_name_tuple[1].replace('/./', '/')
         if not os.path.exists(new_dir_name):
             os.makedirs(new_dir_name, 0755 )
@@ -152,6 +220,8 @@ def detect_codec(bytedata, filename):
         if codec == 'chardet':
             chr_res = chardet.detect(bytedata)
             if not chr_res['encoding'] or chr_res['confidence'] < DEFAULT_CONF['confi_thres']:
+
+                print "The codec of " + filename + " is unable to detect, the result is " + chr_res + " ."
                 return None
             else:
                 return normalize_codec_name(chr_res['encoding'])
---
+++ README.md
@@ -0,0 +1,109 @@
+# Converts text files or source code files into UTF-8 encoding
+
+This lightweight tool converts text files encoded in non-UTF (such as GB2312, GBK, BIG5) to UTF-8 encoded files. 
+It can either be executed from command line interface(a.k.a "CLI" or "console"), or imported into other Python code.
+
+## New Features
+
+- more easier to use
+- support py2
+- has a individual bak folder named with time info
+    
+## Usage
+
+There is only one mandatory argument: filename, where you can specify the directory or file name.  
+
+`-d` to point to the file or directory. If u don't `-d`, it will point to the directory where `2utf` is placed.
+
+* ___Directory mode___: You should put in a directory as the input, and all text files that meets the criteria underneath it will be converted to UTF-8.
+* ___Single file mode___: If the input argument is just an individual file, it would be straightforwardly converted to UTF-8. 
+
+___Examples:___
+
+* Changes all .txt files to UTF-8 encoding. Additionally, **removes BOMs** from utf_8_sig-encoded files: 
+
+    `python 2utf.py`  
+    `python 2utf.py -d "/path/to/your/repo" `
+    
+* Converts an individual file 
+
+    `python 2utf.py -d "/path/to/your/repo/a.txt"`
+
+* remove all .bak files
+
+    `python 2utf.py -c`
+
+* Alternatively, if you are extremely confident with everything, you can simply convert files without creating backups in the beginning.
+    
+    Use the `--nobak` or `-n` option with **extra caution**!
+
+    `python 2utf.py "/path/to/your/repo" --nobak`
+
+* `-t` to import into other Python code. For example, converts all the file to gb2312 : 
+  
+    `python 2utf.py -t gb2312`
+
+* Display help information
+
+    `python 2utf.py -h`
+
+
+## Usage Note
+
+### 1. About BOM
+
+By default, the converted output text files will __NOT__ contain BOM (byte order mark). 
+
+However, you can use the switch `-b` or `--addbom` to explicitly include BOM in the output text files. 
+
+### 2. About file extensions
+
+You should only feed text-like files to `2utf`, while binary files (such as .exe files) **should be** left untouched. 
+However, how to distinguish? Well, we use extension names. By default, files with the extension `txt` will be processed.
+Feel free to customize this list either through editing the source code or with command line arguments.
+
+### 3. About file size limits
+
+We will ignore empty files. Also, we ignore files larger than 10MB. This is a reasonable limit. If you really wants to change it, feel free to do so.
+
+## Trivial knowledge
+
+### 1. About BOM
+To learn more about byte-order-mark (BOM), please check: https://en.wikipedia.org/wiki/Byte_order_mark 
+
+#### 1.1 When should we remove BOM?
+Below is a list of places where BOM might cause a problem. To make your life easy and smooth, BOMs in these files are advised to be removed.
+* __Jekyll__ : Jekyll is a Ruby-based CMS that generates static websites. Please remove BOMs in your source files. Also, remove them in your CSS if you are SASSifying.
+* __PHP__: BOMs in `*.php` files should be stripped.
+* __JSP__: BOMs in `*.jsp` files should be stripped. 
+* (to be added...)
+
+#### 1.2 When should we add BOM?
+BOMs in these files are not necessary, but it is recommended to add them.
+
+* __Source Code in Visual Studio Projects__: 
+    It is recommended in MSDN that "Always prefix a Unicode plain text file with a byte order mark" [Link](https://msdn.microsoft.com/en-us/library/windows/desktop/dd374101(v=vs.85).aspx). 
+    Visual Studio may mandate BOM in source files. If BOMs are missing, then Visual Studio may not be able to compile them.
+
+* __CSV__: 
+    BOMs in CSV files might be useful and necessary, especially if it is opened by Excel.
+
+### 2. About Unicode
+* [其实你并不懂 Unicode by 纤夫张](https://zhuanlan.zhihu.com/p/53714077)
+* [UTF-8 编码及检查其完整性](https://juejin.im/post/5c3ff87f6fb9a049c15f7db0)
+
+
+## FAQ
+
+#### Why do we choose UTF-8 among all charsets? 
+
+For i18n, UTF-8 is wide spread. It is the de-facto standard for non-English texts.
+
+Compared with UTF-16, UTF-8 is usually more compact and "with full fidelity". It also doesn't suffer from the endianness issue of UTF-16. 
+
+#### Why do we need this tool?
+
+Indeed, there are a bunch of text editors out there (such as Notepad++) that handle various encodings of text files very well. Yet for the purpose of __batch conversion__ we need this Python script. This script is also written for educational purpose -- developers can learn from this script to get an idea of how to handle text encoding.
+
+#### Is the current version reliable?
+We are dedicated to deliver quality tools to friends like you. This Python package relies on chardet, which is not so intelligent in terms of recognizing file codecs. Hopefully, better solutions will appear in the future.
